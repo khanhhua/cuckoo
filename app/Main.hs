ab7b11ccab0fab26b6295815f4c5ef0eb0de61a7
Khanh Hua <giakhanh2487@gmail.com>
2022-07-11 22:58:17 +0200

    Use funcor and applicative

--- app/Main.hs
+++ app/Main.hs
@@ -6,9 +6,10 @@ import Control.Monad.State.Lazy
 
 main :: IO ()
 main = do
+  let 
+    fullnameRandomizer = (,) <$> fakeFirstName <*> fakeFamilyName
   g <- newStdGen
-  (fname, nextG) <- runFake fakeFirstName g
-  (lname, _) <- runFake fakeFamilyName nextG
+  ((fname, lname), _nextG) <- runFake fullnameRandomizer g
 
   putStrLn $ fname <> " " <> lname
 
@@ -16,6 +17,19 @@ main = do
 newtype Fake a = Fake
   { runFake :: StdGen -> IO (a, StdGen)
   }
+
+instance Functor Fake where
+  fmap f (Fake randomizer) = Fake $ \g -> do
+    (r, nextG) <- randomizer g
+    pure (f r, nextG)
+
+instance Applicative Fake where
+  pure randomizer = undefined
+  (Fake randomizer1) <*> (Fake randomizer2) = Fake $ \g -> do
+    (f, nextG1) <- randomizer1 g
+    (r, nextG2) <- randomizer2 nextG1
+    pure (f r, nextG2)
+
 
 fakeString :: FilePath -> Fake String
 fakeString p = Fake f
